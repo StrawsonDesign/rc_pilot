72e87783133e4ad3469554081ff9d6fef41c0c9a
James Strawson <james@strawsondesign.com>
2018-05-23 23:48:39 -0700

    savepoint

--- include/mix.h
+++ include/mix.h
@@ -1,13 +1,35 @@
 /**
- * @file mixing_matrix.h
+ * <include/mix.h>
  *
  * @brief      Functions to mix orthogonal inputs to motor controls
+ *
+ *             MultiRotors are controlled by mixing roll, pitch, yaw, and
+ *             throttle control oututs, a linear combination of which forms the
+ *             control output to each motor. The coefficients to this
+ *             combination is stored in a mixing matrix based on rotor layout.
+ *             Also included here are functions to parse configuration strings
+ *             and do the actual mixing.
  */
 
 #ifndef MIXING_MATRIX_H
 #define MIXING_MATRIX_H
 
-#include <mixing_matrix_defs.h>
+#define MAX_INPUTS 6	///< up to 6 control inputs (roll,pitch,yaw,z,x,y)
+#define MAX_ROTORS 8	///< up to 8 rotors
+
+/**
+ * @brief enum for possible mixing matrices defined here
+ *
+ * possible rotor configurations, see mixing_matrix_defs.h
+ */
+typedef enum rotor_layout_t{
+	LAYOUT_4X,
+	LAYOUT_4PLUS,
+	LAYOUT_6X,
+	LAYOUT_8X,
+	LAYOUT_6DOF_ROTORBITS,
+	LAYOUT_6DOF_5INCH_MONOCOQUE
+} rotor_layout_t;
 
 /**
  * @brief      Initiallizes the mixing matrix for a given input layout.
@@ -24,7 +46,7 @@
  *
  * @return     0 on success, -1 on failure
  */
-int initialize_mixing_matrix(rotor_layout_t layout);
+int mix_init(rotor_layout_t layout);
 
 /**
  * @brief      Fills the vector mot with the linear combination of roll pitch
@@ -43,26 +65,27 @@ int initialize_mixing_matrix(rotor_layout_t layout);
  *
  * @return     0 on success, -1 on failure
  */
-int mix_all_controls(float u[6], float* mot);
+int mix_all_controls(double u[6], double* mot);
 
 /**
- * @brief      Checks to see if the desired input u on channel ch would saturate
- *             a motor.
+ * @brief      Finds the min and max inputs u that can be applied to a current
+ *             set of motor outputs before saturating any one motor.
  *
- *             Checks to see if the desired input u on channel ch corresponding
- *             to throttle, roll, pitch, etc would saturate a motor in the given
- *             motor array mot. If saturation would occur, it returns the
- *             maximum allowed input u. Used in mixing_matrix.c
+ *             This is a precurser check to be done before marching a feedback
+ *             controller forward so we know what to saturate the transfer
+ *             function output at. Outputs min and max are given as pointers
+ *             that are written back to. The mot motor array argument is the
+ *             array of current motor outputs that the new channel ch will be
+ *             adding onto.
  *
  * @param[in]  ch    channel
- * @param      mot   The motor signal to be saturated
- * @param      min   The minimum
- * @param      max   The maximum
+ * @param[in]      mot   The array of motor signals to be added onto
+ * @param[out]      min   The minimum possible input without saturation
+ * @param[out]      max   The maximum possible input without saturation
  *
- * @return     maximum allowed input u if saturation occurs. -1 if saturation
- *             does not occur. !!!
+ * @return     0 on success, -1 on failure
  */
-int check_channel_saturation(int ch, float* mot, float* min, float* max);
+int mix_check_saturation(int ch, double* mot, double* min, double* max);
 
 /**
  * @brief      Mixes the control input u for a single channel ch to the existing
@@ -80,7 +103,7 @@ int check_channel_saturation(int ch, float* mot, float* min, float* max);
  *
  * @return     0 on success, -1 on failure
  */
-int add_mixed_input(float u, int ch, float* mot);
+int add_mixed_input(double u, int ch, double* mot);
 
 
 #endif // MIXING_MATRIX_H
