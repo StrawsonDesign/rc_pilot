7c743afe2ec2c9859987edb46eb9ea05ebc82c3b
Glen Haggin <ghaggin@umich.edu>
2019-08-20 08:56:31 -0400

    Changed alt_bmp to state estimate.Z outise of state estimator.

--- src/feedback.c
+++ src/feedback.c
@@ -223,7 +223,7 @@ int feedback_march(void)
     {
         if (last_en_Z_ctrl == 0)
         {
-            setpoint.Z = state_estimate.alt_bmp;  // set altitude setpoint to current altitude
+            setpoint.Z = state_estimate.Z;  // set altitude setpoint to current altitude
             rc_filter_reset(&D_Z);
             tmp = -setpoint.Z_throttle / (cos(state_estimate.roll) * cos(state_estimate.pitch));
             rc_filter_prefill_outputs(&D_Z, tmp);
@@ -231,7 +231,7 @@ int feedback_march(void)
         }
         D_Z.gain = D_Z_gain_orig * settings.v_nominal / state_estimate.v_batt_lp;
         tmp = rc_filter_march(
-            &D_Z, -setpoint.Z + state_estimate.alt_bmp);  // altitude is positive but +Z is down
+            &D_Z, -setpoint.Z + state_estimate.Z);  // altitude is positive but +Z is down
         rc_saturate_double(&tmp, MIN_THRUST_COMPONENT, MAX_THRUST_COMPONENT);
         u[VEC_Z] = tmp / cos(state_estimate.roll) * cos(state_estimate.pitch);
         mix_add_input(u[VEC_Z], VEC_Z, mot);
